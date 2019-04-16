7608e54c799a164510ada27d944ff0e9397b6115
Escarleth Fernandez <escarleth.fernandez5@gmail.com>
2019-04-16 13:02:00 -0400

    changed if statement for styling purpose

--- js/audio.js
+++ js/audio.js
@@ -41,7 +41,10 @@ window.onload = () => {
     let rewindAudio = () => {
         let minutes = Math.floor(audioElement.currentTime / 60);
         let seconds = Math.floor(audioElement.currentTime - minutes * 60);
-        if(seconds > 3)audioElement.currentTime -= 3;
+        
+        if(seconds > 3){
+            audioElement.currentTime -= 3;
+        }
     }
     let setTime = () => {
         "use strict";
